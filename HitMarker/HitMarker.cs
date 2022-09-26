5608f92ae872fe26e4ea3956a509cfac4883bfd8
MaksKesh <91802609+MeguFlint@users.noreply.github.com>
2022-09-26 12:11:59 +0300

    Add files via upload

--- HitMarker/HitMarker.cs
+++ HitMarker/HitMarker.cs
@@ -4,25 +4,37 @@ using Exiled.API.Enums;
 using Exiled.API.Features;
 using Exiled.Events.Extensions;
 using Exiled.API.Interfaces;
+using Exiled.Events.Handlers;
+using Exiled.API.Structs;
+using Exiled.Events.Commands;
+using Exiled.Events.Patches;
 
 namespace HitMarker
 {
     public class Plugin : Plugin<Config>
     {
-        public override string Author { get; } = "KoT0XleB#4663";
+        public override string Author { get; } = "by MrAfitol for Milky";
         public override string Name { get; } = "Hitmarker";
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override string Prefix => "hit_marker";
+        public override Version RequiredExiledVersion => new Version(5, 3, 0);
+        public override Version Version { get; } = new Version(1, 1, 0);
+
         public override void OnEnabled()
         {
             Exiled.Events.Handlers.Player.Hurting += PlayerOnHurting;
         }
+
         public override void OnDisabled()
         {
             Exiled.Events.Handlers.Player.Hurting -= PlayerOnHurting;
         }
+
         public static void PlayerOnHurting(HurtingEventArgs ev)
         {
-                ev.Attacker.ShowHint($"       <size=25><color=blue>X</color></size>  <size=25>[<color=green>{(int)ev.Amount}</color>]\n\n\n\n\n\n\n\n\n\n\n", 2f);
-            }
+            if (ev.Attacker == null) return;
+            if (ev.Target == null) return;
+
+            ev.Attacker.ShowHint($"       <size=25><color=blue>X</color></size>  <size=25>[<color=green>{(int)ev.Amount}</color>]\n\n\n\n\n\n\n\n\n\n\n", 2f);
         }
     }
+}
